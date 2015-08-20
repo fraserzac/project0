2722f2a15123d8d1090e49ea0041ce492b106837
Zachary Fraser <zacharyfraser@Zacharys-MacBook-Pro.local>
2015-08-20 16:29:54 +1000

    updated TTT and javascript files

--- ttt.js
+++ ttt.js
@@ -1,6 +1,7 @@
 var playCounter = 0;
 var currentMove = "X";
 
+
 $('.box').click(function(){
 	if (currentMove === "O") {
 		$(this).addClass('player-o');
@@ -8,10 +9,11 @@ $('.box').click(function(){
 	}	else {
 		$(this).addClass('player-x');
 		$(this).data("player", currentMove);
-	}
+	}	
 
 	isWinner()
 	swapPlayer()
+
 });
 
 var swapPlayer = function () {
@@ -20,13 +22,19 @@ var swapPlayer = function () {
 	} else {
 		currentMove = "X";
 	}
+
 	playCounter++;
 };
 
- 
-$('.replyButton').on('click',function() {
-		;
-	});
+
+// var computer = function( currentMove ) {
+//     if (currentMove = "player-o") {
+       
+//     }
+// };
+
+
+///////////player data and function/////////////
 
 var isWinner = function() {
 	var firstBox = $('#A1').data('player')
@@ -39,32 +47,48 @@ var isWinner = function() {
 	var eighthBox = $('#A8').data('player')
 	var ninthBox = $('#A9').data('player')
 
-	// Store box four, five and sixs data-player
-	// Make a new if statement to check if they are all the same
+var Ai = function() {
+	if ('.box' === currentMove && secondBox !== undefined)
+		alert(currentMove + "wwww")
 
+}	
+
+
+	////////winning possibilities///////
 
 	if (firstBox === secondBox && secondBox === thirdBox && thirdBox !== undefined) {
-		alert(currentMove + "is the Winner")
+		console.log("x is the winner")
+		alert(currentMove + " is the Winner")
 	} else if ( fourthBox === fifthBox && sixthBox === fifthBox && sixthBox !== undefined ) {
-		alert(currentMove + "is the Winner")
+		alert(currentMove + " is the Winner")
 	} else if ( seventhBox === eighthBox && eighthBox === ninthBox && ninthBox !== undefined) {
-		alert(currentMove + "is the winner")
+		alert(currentMove + " is the winner")
 	} else if (firstBox === fourthBox && seventhBox === fourthBox && seventhBox !== undefined) {
-		alert(currentMove + "is the winner")
+		alert(currentMove + " is the winner")
 	} else if (secondBox === fifthBox && eighthBox === fifthBox && eighthBox !== undefined) {
-		alert(currentMove + "is the winner")
+		alert(currentMove + " is the winner")
 	} else if ( thirdBox === sixthBox && ninthBox === ninthBox && ninthBox !== undefined) {
-		alert(currentMove + "is the winner")
+		alert(currentMove + " is the winner")
 	} else if (thirdBox === fifthBox && seventhBox === fifthBox && seventhBox !== undefined) {
-		alert(currentMove + "is the winner")
+		alert(currentMove + " is the winner")
 	} else if (firstBox === fifthBox && ninthBox === fifthBox && ninthBox !== undefined) {
-		alert(currentMove + "is the winner")
+		alert(currentMove + " is the winner")
 	}
 
-	// Store the data player attributes in variables
-	// For each .box
+};
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
